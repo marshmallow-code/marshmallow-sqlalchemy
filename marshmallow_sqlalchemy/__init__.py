e9163dd23f6fbe0d08e7fdabac8011a5b8f54415
Steven Loria <sloria1@gmail.com>
2015-04-26 16:16:29 -0400

    Expose get_pk_from_identity and ModelConverter

--- marshmallow_sqlalchemy/__init__.py
+++ marshmallow_sqlalchemy/__init__.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .schema import fields_for_model, SQLAlchemySchemaOpts, SQLAlchemyModelSchema
+from .schema import (
+    fields_for_model,
+    SQLAlchemySchemaOpts,
+    SQLAlchemyModelSchema,
+    get_pk_from_identity,
+    ModelConverter
+)
 from .exceptions import ModelConversionError
 
 __version__ = '0.1.0.dev'
 __license__ = 'MIT'
 
 __all__ = [
-    'fields_for_model',
-    'SQLAlchemySchemaOpts',
     'SQLAlchemyModelSchema',
+    'SQLAlchemySchemaOpts',
+    'ModelConverter',
+    'fields_for_model',
+    'get_pk_from_identity',
     'ModelConversionError',
 ]
