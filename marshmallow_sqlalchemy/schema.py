0d37666ee690841f4b3a0a2d39e701b78297262f
dpwrussell <douglas_russell@hms.harvard.edu>
2015-08-25 09:11:21 -0400

    setup_schema on 'after_configured' instead

--- marshmallow_sqlalchemy/schema.py
+++ marshmallow_sqlalchemy/schema.py
@@ -68,7 +68,7 @@ class ModelSchema(with_metaclass(SchemaMeta, ma.Schema)):
         return self.opts.model(**data)
 
 
-def setup_schema(session):
+def setup_schema(Base, session):
     """Automatically setup Marshmallow schemas. Designed to trigger from
     SQLAlchemy's 'mapper_configured' event.
 
@@ -93,7 +93,7 @@ def setup_schema(session):
             def __repr__(self):
                 return '<Author(name={self.name!r})>'.format(self=self)
 
-        event.listen(mapper, 'mapper_configured', setup_schema(session))
+        event.listen(mapper, 'after_configured', setup_schema(Base, session))
 
         Base.metadata.create_all(engine)
 
@@ -106,25 +106,27 @@ def setup_schema(session):
         print author_schema.dump(author).data
 
     """
-    def setup_schema_fn(mapper, class_):
-        if class_.__name__.endswith('Schema'):
-            raise ModelConversionError(
-                "For safety, setup_schema can not be used when a Model class"\
-                "ends with 'Schema'"
-            )
+    def setup_schema_fn():
+        for class_ in Base._decl_class_registry.values():
+            if hasattr(class_, '__tablename__'):
+                if class_.__name__.endswith('Schema'):
+                    raise ModelConversionError(
+                        "For safety, setup_schema can not be used when a"\
+                        "Model class ends with 'Schema'"
+                    )
 
-        class Meta(object):
-            model = class_
-            sqla_session = session
+                class Meta(object):
+                    model = class_
+                    sqla_session = session
 
-        schema_class_name = '%sSchema' % class_.__name__
+                schema_class_name = '%sSchema' % class_.__name__
 
-        schema_class = type(
-            schema_class_name,
-            (ModelSchema,),
-            {'Meta': Meta}
-        )
+                schema_class = type(
+                    schema_class_name,
+                    (ModelSchema,),
+                    {'Meta': Meta}
+                )
 
-        setattr(class_, '__marshmallow__', schema_class())
+                setattr(class_, '__marshmallow__', schema_class())
 
     return setup_schema_fn
