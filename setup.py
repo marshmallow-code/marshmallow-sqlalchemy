81dbb8a7178b897b0064fdb699af876647aa360f
Erik Cederstrand <erik@cederstrand.dk>
2019-10-02 07:54:04 +0200

    Don't advertise support for any Python3 version
    This package doesn't support Python 3.5 anymore

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
