c2a9f1b10bb96d4c9bddf06542927a4b1636705e
Steven Loria <sloria1@gmail.com>
2020-02-09 02:35:49 -0500

    Test against Python 3.8

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite="tests",
     project_urls={
