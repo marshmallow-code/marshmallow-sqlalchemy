52a3cf18cdb76af000b874b28c8a8c1335b81854
Steven Loria <sloria1@gmail.com>
2021-12-18 09:29:41 -0500

    Bump version and update changelog

--- src/marshmallow_sqlalchemy/__init__.py
+++ src/marshmallow_sqlalchemy/__init__.py
@@ -15,7 +15,7 @@ from .convert import (
 )
 from .exceptions import ModelConversionError
 
-__version__ = "0.26.1"
+__version__ = "0.27.0"
 __all__ = [
     "SQLAlchemySchema",
     "SQLAlchemyAutoSchema",
